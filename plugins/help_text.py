ed564dcef47c5e5c189015e005f9fce486d07f4d
Kristy-offl <81421572+kristy-offl@users.noreply.github.com>
2021-05-09 20:02:51 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -29,8 +29,8 @@ def send_start(bot, update):
     bot.send_message(
         chat_id=update.chat.id,
         text=script.START_TEXT.format(update.from_user.first_name),
-        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="🚀 CHANNEL 🚀", url="https://t.me/ML_BotUpdates")], [InlineKeyboardButton(text="🚨SUPPORT🚨", url="https://t.me/ML_SupportGroup"),
-                                                    InlineKeyboardButton(text="SHARE ♐️", url="tg://msg?text=%2A%2AHai%20%E2%9D%A4%EF%B8%8F%2C%2A%2A%20%0A__Today%20i%20just%20found%20out%20an%20intresting%20and%20Powerful__%20%2A%2AZee5%20Downloader%20Bot%2A%2A%20__for%20Free%F0%9F%A5%B0.__%20%20%0A%2A%2ABot%20Link%20%3A%20%40Zee5HEXBot%2A%2A%20%F0%9F%94%A5")]]),
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="🚀 My Updates Channel🚀", url="https://t.me/ML_BotUpdates")], [InlineKeyboardButton(text="🚨Support Group🚨", url="https://t.me/ML_SupportGroup"),
+                                                    InlineKeyboardButton(text="Share Meh♐️", url="tg://msg?text=%2A%2AHai%20%E2%9D%A4%EF%B8%8F%2C%2A%2A%20%0A__Today%20i%20just%20found%20out%20an%20intresting%20and%20Powerful__%20%2A%2AZee5%20Downloader%20Bot%2A%2A%20__for%20Free%F0%9F%A5%B0.__%20%20%0A%2A%2ABot%20Link%20%3A%20%40Zee5_MLBot%2A%2A%20%F0%9F%94%A5")]]),
         parse_mode="html",
         disable_web_page_preview=True,
         reply_to_message_id=update.message_id
